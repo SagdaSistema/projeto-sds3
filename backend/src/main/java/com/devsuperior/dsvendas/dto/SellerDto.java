9cc021126c64ee367fa112327a069a22dfb4d02e
Ricardo Reis <sagda.sistema@outlook.com>
2021-05-08 19:53:43 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDto.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDto.java
@@ -22,8 +22,8 @@ public class SellerDto implements Serializable {
 	}
 	
 	public SellerDto(Seller entity) {		
-		id = getId();
-		this.name = getName();
+		id = entity.getId();
+		this.name = entity.getName();
 	}
 
 	public Long getId() {
